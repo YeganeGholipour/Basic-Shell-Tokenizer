5e9e7407804086394ce76d8cc1788aa74b7a63ad
YeganeGholipour <yeganegholiour@gmail.com>
2025-04-23 20:33:12 +0330

    implemented a basic tokenizer TODO: handle single quote input

---
+++ tokenizer.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAXTOKENS 1000 // what we return as tokens 
+#define MAXLEN 100
+#define SPECIALLEN 5
+
+char special_characters[SPECIALLEN] = {'>', '<', '&', '|', '$'};
+
+int tokenize(char argument[], int max_len);
+void printTokens(char *tokens[], int token_num);
+int allocateMemory(char argument[], char *tokens[], int max_tokens, int *token_num);
+void freeMemory(char *tokens[], int token_num);
+bool isspecial(char character);
+bool is_valid_double_operator(char first, char second);
+int handle_double_quotes(char *argument, int max_len);
+int handle_special_characters(char *argument, char character, int max_len);
+
+int main(void) {
+    char *tokens[MAXTOKENS];
+    char arg[MAXLEN];
+    int arg_status, token_status, token_num = 0;
+
+    while ((arg_status = tokenize(arg, MAXLEN)) == 0)
+    {
+        if ((token_status = allocateMemory(arg, tokens, MAXTOKENS, &token_num)) < 0) {
+            printf("Error While Allocating Memory!");
+            return 1;
+        }
+    }
+
+    printTokens(tokens, token_num);
+    freeMemory(tokens, token_num);
+
+    return 0;
+}
+
+void freeMemory(char *tokens[], int token_num) {
+    for (int i = 0; i < token_num; i++) 
+        free(tokens[i]);
+}
+
+void printTokens(char *tokens[], int token_num) {
+    printf("****************************\n");
+    printf("CALLING FUNCTION: printTokens\n");
+    for (int i = 0; i < token_num; i++)
+        printf("%s\n", tokens[i]);
+}
+
+int allocateMemory(char argument[], char *tokens[], int max_tokens, int *token_num) {
+    char *p;
+    if (*token_num < max_tokens && ((p = (char *)malloc(strlen(argument)+1)) != NULL)) {
+        printf("****************************\n");
+        printf("CALLING FUNCTION: allocateMemory\n");
+        printf("ARGUMENT: %s\n", argument);
+        strcpy(p, argument);
+        tokens[(*token_num)++] = p;
+    } else
+        return -1;
+
+    return 0;
+}   
+
+int tokenize(char argument[], int max_len) {
+    int c;
+
+    // skip the whitespaces
+    while (isspace(c = getchar()))
+        ;
+    
+    if (c == EOF)
+        return EOF;
+
+    /* Process the first non space character */
+    /* handle double quotes and escape characters */
+    if (c == '"') {
+        printf("********************************\n");
+        printf("HANDLING DOUBLE QUOTES\n");
+        return handle_double_quotes(argument, max_len); 
+    }
+
+    /* Process Special Characters */
+    if (isspecial(c)) {
+        printf("********************************\n");
+        printf("HANDLING SPECIAL CHARACTERS\n");
+        return handle_special_characters(argument, c, max_len);
+    }
+
+    /* Process normal characters: A-Z, a-Z, 1-9, / */   
+    else {
+        printf("********************************\n");
+        printf("HANDLING NON SPECIAL CHARACTERS\n");
+        while (max_len > 1 && !isspecial(c) && !isspace(c)) {
+            *argument++ = c;
+            c = getchar();
+        }
+        ungetc(c, stdin); //put c back to buffer to process it the next iteration
+    }
+
+    *argument = '\0';
+    return 0; 
+}
+
+bool isspecial(char character) {
+    for (int i = 0; i < SPECIALLEN; i++)
+        if (character == special_characters[i])
+            return true;
+
+    return false;
+}
+
+int handle_special_characters(char *argument, char character, int max_len) {
+    int c;
+    if (max_len > 1) {
+        *argument++ = character;
+        max_len--;
+
+        c = getchar();
+        if (max_len > 1 && is_valid_double_operator(character, c)) {
+            *argument++ = c;
+            max_len--;
+        } else if (c == EOF) 
+            return EOF;
+        else
+            ungetc(c, stdin);
+
+        *argument = '\0';
+        return 0;
+    } else
+        return -1;
+}
+
+bool is_valid_double_operator(char first, char second) {
+    return (first == '>' && second == '>') ||
+           (first == '<' && second == '<') ||
+           (first == '&' && second == '&') ||
+           (first == '|' && second == '|');
+}
+
+
+int handle_double_quotes(char *argument, int max_len) {
+    int c, next;
+
+    while (max_len > 1 && (c = getchar()) != EOF) {
+        if (c == '\\') {
+            next = getchar();
+            *argument++ = next;
+            max_len--;
+        } 
+        else if (c == '"')
+            break;
+        else {
+            *argument++ = c;
+            max_len--;
+        }
+    }
+
+    *argument = '\0';
+    return (c == EOF) ? EOF : 0;
+}
